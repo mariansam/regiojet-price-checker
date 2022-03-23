d2315464e18bbf6495830d3d682969c1cdbddf02
Marian Šámal <margotka000@gmail.com>
2022-03-23 21:18:22 +0100

    Add code

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import { SeatClassKey } from './types.ts';
+
+export const REQUEST_URL = 'https://brn-ybus-pubapi.sa.cz/restapi/routes/5956372401/simple?fromStationId=372825000&routeId=5956372401&tariffs=CZECH_STUDENT_PASS_15&toStationId=1841058000';
+
+export const SEAT_CLASS: SeatClassKey = 'C0';
+
+export const USE_CREDIT_PRICE = true;
+
+export const OUTPUT_FILE = 'data.txt';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
+import { request } from './request.ts';
+import { Route } from './types.ts';
+import { REQUEST_URL, SEAT_CLASS, USE_CREDIT_PRICE, OUTPUT_FILE } from './config.ts';
+
+const assert = (condition: any, msg: string, code: number) => {
+    if (!condition) {
+        console.error(msg);
+        Deno.exit(code);
+    }
+};
+
+const daysDifference = (startDate: Date, endDate: Date): number => {
+    // clone them because of mutability
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    start.setHours(0, 0, 0, 0);
+    end.setHours(0, 0, 0, 0);
+
+    const diffMs = end.getTime() - start.getTime();
+
+    const diffDays = Math.ceil(diffMs / (1000*60*60*24));
+
+    return diffDays;
+};
+
 console.log('hello from deno!');
+
+const route = await request<Route>(REQUEST_URL, { headers: { 'X-Currency': 'CZK' }});
+
+assert(route.bookable, 'The given route is not bookable, exiting', 1);
+
+assert(route.freeSeatsCount > 0, 'The given route has less than one free seats, exiting', 2);
+console.log(route);
+
+const priceClass = route.priceClasses.find(priceClass => priceClass.seatClassKey === SEAT_CLASS);
+// console.log(priceClass);
+
+assert(priceClass, 'The given route doesnt contain the specified seat class, exiting', 3);
+
+assert(priceClass!.bookable, 'The given seat class is not bookable, exiting', 4);
+
+assert(priceClass!.freeSeatsCount > 0, 'The given seat class has less than one free seats, exiting', 5);
+
+const price = USE_CREDIT_PRICE ? priceClass!.creditPrice : priceClass!.price;
+
+const departureDate = new Date(route.departureTime);
+const now = new Date();
+
+const daysDiff = daysDifference(now, departureDate);
+
+const line = `${daysDiff}\t${now.getHours()}:${now.getMinutes()}\t${price}\n`;
+
+console.log(line);
+await Deno.writeTextFile(OUTPUT_FILE, line, { append: true });
